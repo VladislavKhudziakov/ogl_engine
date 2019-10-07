f757e0b771fee7bb84f7ebf5ef1892c2c473d705
Vladislav Khudziakov <vladislav.khudiakov@banuba.com>
2019-10-07 17:03:48 +0300

    resctructured project

--- configure.py
+++ configure.py
@@ -1,6 +1,6 @@
 import os
 
-LIBS_PATH = "../internal/libs"
+LIBS_PATH = "./internal/libs"
 GLM_URL = "https://github.com/g-truc/glm.git"
 GLFW_URL = "https://github.com/glfw/glfw.git"
 GLAD_URL = "https://github.com/Dav1dde/glad.git"
@@ -8,6 +8,7 @@ ASSIMP_URL = "https://github.com/assimp/assimp.git"
 
 
 def main():
+    os.system('ls')
     install_dependencies()
 #     TODO: ADD configure commands here
 
@@ -16,16 +17,16 @@ def install_dependencies():
     dirs = os.listdir(LIBS_PATH)
 
     if 'glm' not in dirs:
-        os.system("git clone " + GLM_URL + " " + LIBS_PATH)
+        os.system("git clone " + GLM_URL + " " + LIBS_PATH + '/glm')
 
     if 'glfw' not in dirs:
-        os.system("git clone " + GLFW_URL + " " + LIBS_PATH)
+        os.system("git clone " + GLFW_URL + " " + LIBS_PATH + '/glfw')
 
     if 'glad' not in dirs:
-        os.system("git clone " + GLAD_URL + " " + LIBS_PATH)
+        os.system("git clone " + GLAD_URL + " " + LIBS_PATH + '/glad')
 
     if 'assimp' not in dirs:
-        os.system("git clone " + ASSIMP_URL + " " + LIBS_PATH)
+        os.system("git clone " + ASSIMP_URL + " " + LIBS_PATH + '/assimp')
 
 
 main()
