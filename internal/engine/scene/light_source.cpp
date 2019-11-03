cb178a73db5573a14b901aec07cb3b1f67c8ffce
Vladislav Khudziakov <super.tawer.822@gmail.com>
2019-11-03 22:01:01 +0300

    added light source struct

---
+++ internal/engine/scene/light_source.cpp
@@ -0,0 +1,5 @@
+//
+// Created by movleaxedx on 3.11.19.
+//
+
+#include "light_source.hpp"
---
+++ internal/engine/scene/light_source.hpp
@@ -0,0 +1,18 @@
+//
+// Created by movleaxedx on 3.11.19.
+//
+
+
+#pragma once
+
+#include <glm/vec3.hpp>
+
+namespace engine
+{
+    struct light_source
+    {
+        glm::vec3 position;
+        glm::vec3 color;
+    };
+}
+
